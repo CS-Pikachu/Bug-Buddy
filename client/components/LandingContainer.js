82c35e7132a884d6bb310d5984020088d9464f8d
Erik <erikmat@berkeley.edu>
2021-06-21 14:06:16 -0700

    added boilerplate for landing

---
+++ client/components/LandingContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const LandingContainer = () => {
+  return (
+    <div>
+      <h1>from landing</h1>
+    </div>
+  );
+};
+
+export default LandingContainer;
---
+++ client/components/app.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+// import { connect } from 'react-redux';
+// import * as actions from '../actions'; // make sure to look at route;
+// import styles from './../public/bootstrap.css';
+// import { Card, Nav } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+
+// import Header from './Header';
+import LandingContainer from './LandingContainer.js';
+import DashBoard from './DashBoard.js';
+import NavBar from './NavBar.js';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {}
+  render() {
+    return (
+      <Container>
+        <div>
+          <NavBar />
+          <LandingContainer />
+        </div>
+      </Container>
+    );
+  }
+}
+
+// first arg is map state to props, second is action creaters
+// all these actions are assigned to App as props
+export default App;
