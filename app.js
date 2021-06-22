765ac95de390d7364b206f2729be65857651930b
nonstdout <csalisbury@natilik.com>
2021-06-23 00:22:51 +0100

    working modular sqlize

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express')
-import models from './db/models'
-// const mountRoutes = require('./routes')
 const app = express()
-// mountRoutes(app)
-models.sequelize.sync().then(() => {
-    // app.listen(3000);
+const sequelize = require('./db/index')
+
+sequelize.sync().then(() => {
+    app.listen(3000);
 })
 
 
---
+++ db/index.js
@@ -0,0 +1,63 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize(
+    'postgres://fkhcjcfy:4muOB1ZJlD9JQ8KKa9scGtwCdw8H_v_S@tai.db.elephantsql.com/fkhcjcfy', {
+})
+const user = require('./models/User');
+const team = require('./models/Team');
+const bug = require('./models/Bug');
+const comment = require('./models/Comment');
+
+const User = user(sequelize, DataTypes)
+const Team = team(sequelize, DataTypes)
+const Bug = bug(sequelize, DataTypes)
+const Comment = comment(sequelize, DataTypes)
+
+const models = {
+    User,
+    Team,
+    Bug,
+    Comment,
+}
+
+Object.keys(models).forEach(key => {
+    if (models[key].associate) {
+        models[key].associate(models)
+    }
+})
+
+
+module.exports = sequelize;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// async function getOnePerson() {
+//     let person = await Person.findOne()
+//     console.log(person.get('name'))
+// }
+
+// getOnePerson()
+
+// async function createUser() {
+    // await User.sync({ force: true })
+//     const jane = await User.create({ name: "Jane" });
+//     console.log("Jane's auto-generated ID:", jane.id);
+
+// }
+// createUser()
