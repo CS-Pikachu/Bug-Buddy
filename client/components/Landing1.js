8d55d4c3b6fded51ef8bf2059c3476a20a1885e2
Carlos Botero-Vargas <80601385+Carlos-BoteroVargas@users.noreply.github.com>
2021-06-22 17:12:17 -0500

    image loader now serving local resources

--- client/components/Landing1.js
+++ client/components/Landing1.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
+import statistics from '../assets/statistics.svg';
 
 const Landing1 = () => {
     return (
-        <div class="row">
-          <div class="col-sm-6">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Have a bug? No Problem!</h5>
-                <p class="card-text">As your organization grows, IT support has to be more organized and efficient. Without a clear overview of what the problem is, who's reporting it, and what its priority level is, it takes more time to resolve IT issues — as a result, operations will be bottlenecked within the company.</p>
-                <a href="#" class="btn btn-primary">Get Started</a>
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Have a bug? No Problem!</h5>
+                <p className="card-text">As your organization grows, IT support has to be more organized and efficient. Without a clear overview of what the problem is, who's reporting it, and what its priority level is, it takes more time to resolve IT issues — as a result, operations will be bottlenecked within the company.</p>
+                <a href="#" className="btn btn-primary">Get Started</a>
               </div>
             </div>
           </div>
-          <div class="col-sm-6">
-            <div class="card">
-              <div class="card-body">
-              <img src='https://raw.githubusercontent.com/CS-Pikachu/Bug-Buddy/42c4b009b321cf32f84a166fe6d591533777db09/client/assets/statistics.svg' alt="landing" width="415" height="250"></img>
+          <div className="col-sm-6">
+            <div className="card">
+              <div className="card-body">
+              <img src={statistics} alt="landing" width="415" height="250"></img>
               </div>
             </div>
           </div>
