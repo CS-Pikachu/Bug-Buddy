867b0bcd3b3384c096507c2c81f8ff2513eb9ad0
nonstdout <csalisbury@natilik.com>
2021-06-22 22:32:19 +0100

    working sql sync single file

--- db/models/user.js
+++ db/models/user.js
@@ -1,18 +1,14 @@
-export default (sequelize, DataTypes) => {
-    const User = sequelize.define('user', {
-        username: {
-            type: DataTypes.STRING,
-            unique: true,
-        },
-        password: DataTypes.STRING,
-        isAdmin: DataTypes.BOOLEAN,
-    });
-    User.associate = (models) => {
-        User.belongsToMany(models.Team, {
-            through: 'member',
-            foreighKey: 'userId',
-        })
-    };
-    console.log('userjs')
-    return User;
+const User = sequelize.define('user', {
+    username: {
+        type: DataTypes.STRING,
+        unique: true,
+    },
+    password: DataTypes.STRING,
+    isAdmin: DataTypes.BOOLEAN,
+});
+User.associate = (models) => {
+    User.belongsToMany(models.Team, {
+        through: 'member',
+        foreighKey: 'userId',
+    })
 };
