cfe5b61b10114900000850bfd36e6a7977999558
Carlos Botero-Vargas <80601385+Carlos-BoteroVargas@users.noreply.github.com>
2021-06-22 19:40:38 -0500

    minor cleanup

--- db/models/user.js
+++ db/models/user.js
@@ -1,17 +1,17 @@
 module.exports = function (sequelize, DataTypes) {
-    const User = sequelize.define('user', {
-        username: {
-            type: DataTypes.STRING,
-            unique: true,
-        },
-        password: DataTypes.STRING,
-        isAdmin: DataTypes.BOOLEAN,
-    });
-    User.associate = function (models) {
-        User.belongsToMany(models.Team, {
-            through: 'member',
-            foreignKey: 'userId',
-        })
-    }
-    return User;
+  const User = sequelize.define('user', {
+      username: {
+          type: DataTypes.STRING,
+          unique: true,
+      },
+      password: DataTypes.STRING,
+      isAdmin: DataTypes.BOOLEAN,
+  });
+  User.associate = function (models) {
+      User.belongsToMany(models.Team, {
+          through: 'member',
+          foreignKey: 'userId',
+      })
+  }
+  return User;
 }
