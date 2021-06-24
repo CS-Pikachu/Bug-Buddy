272576459af2c00bb8555644d83a8b11a9603ccd
Erik <erikmat@berkeley.edu>
2021-06-24 16:10:29 -0700

    done function completed

--- client/actions/types.js
+++ client/actions/types.js
@@ -2,3 +2,4 @@ export const FETCH_USER = 'fetch_user';
 export const FETCH_BUGS = 'fetch_bugs';
 export const FETCH_COMMENTS = 'fetch_comments';
 export const FETCH_ALL_USERS = 'fetch_all-users';
+export const UPDATE_BUG = 'update_bug';
